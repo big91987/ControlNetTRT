6657a0518c6d15fc5d2588ff84fdd1d37ff4ef9d
Jiuzhou.Zhao <Jiuzhou.Zhao@streamcomputing.com>
2023-08-11 07:20:36 +0800

    123

--- canny2image_TRT.py
+++ canny2image_TRT.py
@@ -212,7 +212,7 @@ class hackathon():
 
     def process(self, input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold):
         
-        ddim_steps = 8
+        ddim_steps = 9
         with torch.no_grad():
             img = resize_image(HWC3(input_image), image_resolution)
             H, W, C = img.shape
